3027c3c087552dff3c081baacaded960aa38f5f9
Lily <bloomyday56@gmail.com>
2024-02-26 12:45:46 -0800

    fix typo in server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret: 'process.env.SECRET',
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true
 }))
